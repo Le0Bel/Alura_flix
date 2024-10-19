40a18a3bcf02fa80557bc1e2d4a80b09aa468d74
FacundoBe <facundobellomo@gmail.com>
2024-10-18 23:35:06 -0300

    agragada clase preview  a la tarjete de curso en el modo edicion

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -10,17 +10,17 @@ export default function Course({ id, name, description, image, duration, level, 
     const { user } = useContext(AuthContext)
 
     return (
-        <div className="course-card" onClick={()=> selectActiveCourse(id)}>
-            <h1 className="course-title">{name}</h1>
-            <div className="course-main-top">
-                <p className="course-description">{description} </p>
-                <div className="course-logo">
-                    <img src={image} alt="" />
+        <div className={`course-card ${!editable ? "preview" : ""}`} onClick={()=> selectActiveCourse(id)}>
+            <h1 className={`course-title ${!editable ? "preview" : ""}`}>{name}</h1>
+            <div className={`course-main-top ${!editable ? "preview" : ""}`}>
+                <p className={`course-description ${!editable ? "preview" : ""}`}>{description} </p>
+                <div className={`course-logo ${!editable ? "preview" : ""}`}>
+                    <img src={image}  />
                 </div>
             </div>
-            <div className="course-main-bottom">
-                <p className="course-main-bottom-duration">Duracion: <span>{duration}</span></p>
-                <p className="course-main-bottom-nivel">Nivel: <span>{level}</span></p>
+            <div className={`course-main-bottom ${!editable ? "preview" : ""}`}>
+                <p className={`course-main-bottom-duration ${!editable ? "preview" : ""}`}>Duracion: <span>{duration}</span></p>
+                <p className={`course-main-bottom-nivel ${!editable ? "preview" : ""}`}>Nivel: <span>{level}</span></p>
             </div>
             {user.role === "admin" && editable &&
                 <div className='card-action-course' >
